0234b7782d43002456bc44bc8e9c759fce7a0451
Aryan <aryan.soni@btech.christuniversity.in>
2020-03-20 22:34:11 +0530

    added css

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app = express();
 //     next();
 // });
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(Route404);
