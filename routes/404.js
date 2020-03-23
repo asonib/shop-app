2cd399b9ed66a7bce50400ea8774399979c8bc92
Aryan <aryan.soni@btech.christuniversity.in>
2020-03-23 09:38:30 +0530

    pug implementation completed

--- routes/404.js
+++ routes/404.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const path = require('path');
 
 router.use((req, res, next) =>{
-    res.status(404).sendFile(path.join(__dirname, "../", "views", "404.html"));
+    //res.status(404).sendFile(path.join(__dirname, "../", "views", "404.html"));
     //res.render('404');
+    res.status(404).render('404');
 });
 module.exports = router;
